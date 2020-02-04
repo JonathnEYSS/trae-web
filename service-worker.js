47358f7c73c7c3d3bad5da00410461f0dc78638a
Jonathan <jonathn.jpr@gmail.com>
2020-02-04 18:41:20 -0400

    04-02

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ee08b657385ed87afa1619c040ac2f9.js"
+  "/precache-manifest.8050d8fdb7d4341827a4b6ff4a043ac7.js"
 );
 
 self.addEventListener('message', (event) => {
